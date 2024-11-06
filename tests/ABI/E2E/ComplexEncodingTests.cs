ff71179721c16d194cb1dd599f7a95675c29aea1
leonlolly <leon@wenderoth.de>
2024-11-06 19:46:58 +0100

    feat: add more tests

---
+++ tests/ABI/E2E/ComplexEncodingTests.cs
@@ -0,0 +1,33 @@
+﻿using EtherSharp.ABI;
+using EtherSharp.ABI.Decode;
+
+namespace EtherSharp.Tests.ABI.E2E;
+public class ComplexEncodingTests
+{
+
+    [Fact]
+    public void Multible_Numbers()
+    {
+        var abiEncoder = new AbiEncoder()
+            .UInt8(byte.MaxValue)
+            .UInt8(byte.MaxValue)
+            .UInt8(50)
+            .UInt8(byte.MinValue)
+            .UInt8(byte.MinValue);
+        ;
+        byte[] buffer = abiEncoder.Build();
+
+        _ = new AbiDecoder(buffer)
+            .UInt8(out byte a)
+            .UInt8(out byte b)
+            .UInt8(out byte c)
+            .UInt8(out byte d)
+            .UInt8(out byte e);
+
+        Assert.Equal(byte.MaxValue, a);
+        Assert.Equal(byte.MaxValue, b);
+        Assert.Equal(50, c);
+        Assert.Equal(byte.MinValue, d);
+        Assert.Equal(byte.MinValue, e);
+    }
+}
---
+++ tests/ABI/E2E/End2EndTestSetOne.cs
@@ -0,0 +1,40 @@
+﻿using EtherSharp.ABI;
+using EtherSharp.ABI.Decode;
+
+namespace EtherSharp.Tests.ABI.E2E;
+
+public class End2EndTests
+{
+    [Fact]
+    public void Should_Encode_And_Decode_Int8_Array()
+    {
+        // Arrange
+        sbyte[] input = { 1, 2, 3, 4, 5 };
+
+        // Act
+        byte[] encoded = new AbiEncoder().NumberArray<sbyte>(false, 8, input).Build();
+        var decoder = new AbiDecoder(encoded);
+        _ = decoder.NumberArray<sbyte>(false, 8, out sbyte[] result);
+
+        // Assert
+        Assert.Equal(input, result);
+    }
+
+    [Fact]
+    public void Should_Encode_And_Decode_UInt16_String()
+    {
+        // Arrange
+        ushort number = 16;
+        string str = "Hello, World!";
+
+        // Act
+        byte[] encoded = new AbiEncoder().Number<ushort>(number, true, 16).String(str).Build();
+        var decoder = new AbiDecoder(encoded);
+        _ = decoder.Number<ushort>(out ushort outputNumber, true, 16);
+        _ = decoder.String(out string outputStr);
+
+        // Assert
+        Assert.Equal(number, outputNumber);
+        Assert.Equal(str, outputStr);
+    }
+}
