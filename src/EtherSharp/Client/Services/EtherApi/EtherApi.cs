960aec19903941691c573fb16f1aaf32eb0817a2
PoolPirate <eliaswolf1609@gmail.com>
2024-11-29 12:27:03 +0100

    feat: Add overloads using Address type

--- src/EtherSharp/Client/Services/EtherApi/EtherApi.cs
+++ src/EtherSharp/Client/Services/EtherApi/EtherApi.cs
@@ -9,7 +9,11 @@ internal class EtherApi(EvmRpcClient rpcClient) : IEtherTxApi
     private readonly EvmRpcClient _rpcClient = rpcClient;
 
     public Task<BigInteger> GetBalanceAsync(string address, TargetBlockNumber blockNumber)
-        => _rpcClient.EthGetBalance(address, blockNumber);
+        => _rpcClient.EthGetBalance(address, blockNumber); 
+    public Task<BigInteger> GetBalanceAsync(Address address, TargetBlockNumber blockNumber)
+        => _rpcClient.EthGetBalance(address.String, blockNumber);
     public TxInput Transfer(string receiver, BigInteger amount)
         => TxInput.ForEthTransfer(Address.FromString(receiver), amount);
+    public TxInput Transfer(Address receiver, BigInteger amount)
+        => TxInput.ForEthTransfer(receiver, amount);
 }
--- src/EtherSharp/Client/Services/EtherApi/IEtherTxApi.cs
+++ src/EtherSharp/Client/Services/EtherApi/IEtherTxApi.cs
@@ -1,8 +1,10 @@
 ﻿using EtherSharp.Tx;
+using EtherSharp.Types;
 using System.Numerics;
 
 namespace EtherSharp.Client.Services.EtherApi;
 public interface IEtherTxApi : IEtherApi
 {
     public TxInput Transfer(string receiver, BigInteger amount);
+    public TxInput Transfer(Address receiver, BigInteger amount);
 }
