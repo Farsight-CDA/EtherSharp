404bffbb3e5795e83aa0a5f9b79486c6f5eb4870
PoolPirate <eliaswolf1609@gmail.com>
2024-11-29 12:29:41 +0100

    feat: Add GetMyBalanceAsync overload

--- src/EtherSharp/Client/Services/EtherApi/EtherApi.cs
+++ src/EtherSharp/Client/Services/EtherApi/EtherApi.cs
@@ -1,17 +1,25 @@
 ﻿using EtherSharp.RPC;
 using EtherSharp.Tx;
 using EtherSharp.Types;
+using EtherSharp.Wallet;
+using Microsoft.Extensions.DependencyInjection;
 using System.Numerics;
 
 namespace EtherSharp.Client.Services.EtherApi;
-internal class EtherApi(EvmRpcClient rpcClient) : IEtherTxApi
+internal class EtherApi(EvmRpcClient rpcClient, IServiceProvider provider) : IEtherTxApi
 {
     private readonly EvmRpcClient _rpcClient = rpcClient;
+    private readonly IServiceProvider _provider = provider;
 
     public Task<BigInteger> GetBalanceAsync(string address, TargetBlockNumber blockNumber)
         => _rpcClient.EthGetBalance(address, blockNumber); 
     public Task<BigInteger> GetBalanceAsync(Address address, TargetBlockNumber blockNumber)
         => _rpcClient.EthGetBalance(address.String, blockNumber);
+    public Task<BigInteger> GetMyBalanceAsync(TargetBlockNumber blockNumber)
+        => _rpcClient.EthGetBalance(
+            _provider.GetService<IEtherSigner>()?.Address?.String ?? throw new InvalidOperationException("Client is not a tx client")
+            blockNumber
+        );
     public TxInput Transfer(string receiver, BigInteger amount)
         => TxInput.ForEthTransfer(Address.FromString(receiver), amount);
     public TxInput Transfer(Address receiver, BigInteger amount)
--- src/EtherSharp/Client/Services/EtherApi/IEtherTxApi.cs
+++ src/EtherSharp/Client/Services/EtherApi/IEtherTxApi.cs
@@ -5,6 +5,8 @@ using System.Numerics;
 namespace EtherSharp.Client.Services.EtherApi;
 public interface IEtherTxApi : IEtherApi
 {
+    public Task<BigInteger> GetMyBalanceAsync(TargetBlockNumber blockNumber = default);
+
     public TxInput Transfer(string receiver, BigInteger amount);
     public TxInput Transfer(Address receiver, BigInteger amount);
 }
