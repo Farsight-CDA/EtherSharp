b41310eb199f3b539b5d448083fcdeb0002de2c2
PoolPirate <eliaswolf1609@gmail.com>
2024-12-01 17:30:51 +0100

    feat: Sized bytes in tuples

---
+++ src/EtherSharp/ABI/Encode/Interfaces/IDynamicTupleEncoder.SizedBytes.cs
@@ -0,0 +1,39 @@
+using EtherSharp.ABI.Fixed;
+
+namespace EtherSharp.ABI.Encode.Interfaces;
+public partial interface IDynamicTupleEncoder 
+{
+    public IDynamicTupleEncoder Bytes1(byte value);
+    public IDynamicTupleEncoder Bytes2(params byte[] value);
+    public IDynamicTupleEncoder Bytes3(params byte[] value);
+    public IDynamicTupleEncoder Bytes4(params byte[] value);
+    public IDynamicTupleEncoder Bytes5(params byte[] value);
+    public IDynamicTupleEncoder Bytes6(params byte[] value);
+    public IDynamicTupleEncoder Bytes7(params byte[] value);
+    public IDynamicTupleEncoder Bytes8(params byte[] value);
+    public IDynamicTupleEncoder Bytes9(params byte[] value);
+    public IDynamicTupleEncoder Bytes10(params byte[] value);
+    public IDynamicTupleEncoder Bytes11(params byte[] value);
+    public IDynamicTupleEncoder Bytes12(params byte[] value);
+    public IDynamicTupleEncoder Bytes13(params byte[] value);
+    public IDynamicTupleEncoder Bytes14(params byte[] value);
+    public IDynamicTupleEncoder Bytes15(params byte[] value);
+    public IDynamicTupleEncoder Bytes16(params byte[] value);
+    public IDynamicTupleEncoder Bytes17(params byte[] value);
+    public IDynamicTupleEncoder Bytes18(params byte[] value);
+    public IDynamicTupleEncoder Bytes19(params byte[] value);
+    public IDynamicTupleEncoder Bytes20(params byte[] value);
+    public IDynamicTupleEncoder Bytes21(params byte[] value);
+    public IDynamicTupleEncoder Bytes22(params byte[] value);
+    public IDynamicTupleEncoder Bytes23(params byte[] value);
+    public IDynamicTupleEncoder Bytes24(params byte[] value);
+    public IDynamicTupleEncoder Bytes25(params byte[] value);
+    public IDynamicTupleEncoder Bytes26(params byte[] value);
+    public IDynamicTupleEncoder Bytes27(params byte[] value);
+    public IDynamicTupleEncoder Bytes28(params byte[] value);
+    public IDynamicTupleEncoder Bytes29(params byte[] value);
+    public IDynamicTupleEncoder Bytes30(params byte[] value);
+    public IDynamicTupleEncoder Bytes31(params byte[] value);
+    public IDynamicTupleEncoder Bytes32(params byte[] value);
+
+}
---
+++ src/EtherSharp/ABI/Encode/Interfaces/IFixedTupleEncoder.SizedBytes.cs
@@ -0,0 +1,39 @@
+using EtherSharp.ABI.Fixed;
+
+namespace EtherSharp.ABI.Encode.Interfaces;
+public partial interface IFixedTupleEncoder 
+{
+    public IFixedTupleEncoder Bytes1(byte value);
+    public IFixedTupleEncoder Bytes2(params byte[] value);
+    public IFixedTupleEncoder Bytes3(params byte[] value);
+    public IFixedTupleEncoder Bytes4(params byte[] value);
+    public IFixedTupleEncoder Bytes5(params byte[] value);
+    public IFixedTupleEncoder Bytes6(params byte[] value);
+    public IFixedTupleEncoder Bytes7(params byte[] value);
+    public IFixedTupleEncoder Bytes8(params byte[] value);
+    public IFixedTupleEncoder Bytes9(params byte[] value);
+    public IFixedTupleEncoder Bytes10(params byte[] value);
+    public IFixedTupleEncoder Bytes11(params byte[] value);
+    public IFixedTupleEncoder Bytes12(params byte[] value);
+    public IFixedTupleEncoder Bytes13(params byte[] value);
+    public IFixedTupleEncoder Bytes14(params byte[] value);
+    public IFixedTupleEncoder Bytes15(params byte[] value);
+    public IFixedTupleEncoder Bytes16(params byte[] value);
+    public IFixedTupleEncoder Bytes17(params byte[] value);
+    public IFixedTupleEncoder Bytes18(params byte[] value);
+    public IFixedTupleEncoder Bytes19(params byte[] value);
+    public IFixedTupleEncoder Bytes20(params byte[] value);
+    public IFixedTupleEncoder Bytes21(params byte[] value);
+    public IFixedTupleEncoder Bytes22(params byte[] value);
+    public IFixedTupleEncoder Bytes23(params byte[] value);
+    public IFixedTupleEncoder Bytes24(params byte[] value);
+    public IFixedTupleEncoder Bytes25(params byte[] value);
+    public IFixedTupleEncoder Bytes26(params byte[] value);
+    public IFixedTupleEncoder Bytes27(params byte[] value);
+    public IFixedTupleEncoder Bytes28(params byte[] value);
+    public IFixedTupleEncoder Bytes29(params byte[] value);
+    public IFixedTupleEncoder Bytes30(params byte[] value);
+    public IFixedTupleEncoder Bytes31(params byte[] value);
+    public IFixedTupleEncoder Bytes32(params byte[] value);
+
+}
