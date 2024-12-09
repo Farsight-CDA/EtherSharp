fad811b4caaafe7f26682a26cc64c7a274378aa2
Playwo <eliaswolf2001@t-online.de>
2024-12-09 11:14:52 +0000

    fix: id parsing

--- src/EtherSharp/Transport/WssJsonRpcTransport.cs
+++ src/EtherSharp/Transport/WssJsonRpcTransport.cs
@@ -21,7 +21,7 @@ public class WssJsonRpcTransport(Uri uri) : IRPCTransport
     public Task<RpcResult<TResult>> SendRpcRequest<T1, TResult>(string method, T1 t1)
         => InnerSendAsync<TResult>(method, [t1], default);
     public Task<RpcResult<TResult>> SendRpcRequest<T1, T2, TResult>(string method, T1 t1, T2 t2)
-        => InnerSendAsync<TResult>(method, [t1, t2], default); 
+        => InnerSendAsync<TResult>(method, [t1, t2], default);
     public Task<RpcResult<TResult>> SendRpcRequest<T1, T2, T3, TResult>(string method, T1 t1, T2 t2, T3 t3)
         => InnerSendAsync<TResult>(method, [t1, t2, t3], default);
 
@@ -39,30 +39,44 @@ public class WssJsonRpcTransport(Uri uri) : IRPCTransport
         using var ms = new MemoryStream();
         byte[] buffer = new byte[8192];
 
-        while(_socket.State == WebSocketState.Open)
+        try
         {
-            WebSocketReceiveResult receiveResult;
 
-            do
+            while(_socket.State == WebSocketState.Open)
             {
-                receiveResult = await _socket.ReceiveAsync(buffer, default);
+                WebSocketReceiveResult receiveResult;
 
-                if(receiveResult.Count != 0)
+                do
                 {
-                    ms.Write(buffer.AsSpan()[0..receiveResult.Count]);
+                    receiveResult = await _socket.ReceiveAsync(buffer, default);
+
+                    if(receiveResult.Count != 0)
+                    {
+                        ms.Write(buffer.AsSpan()[0..receiveResult.Count]);
+                    }
                 }
+                while(!receiveResult.EndOfMessage);
+
+                var msBuffer = ms.GetBuffer().AsSpan()[0..(int) ms.Length];
+                int requestId = ExtractRequestIdFromJson(msBuffer);
+
+                var (_, responseType, tcs) = _pendingRequests.First(x => x.RequestId == requestId);
+                object? response = JsonSerializer.Deserialize(
+                    msBuffer, responseType, 
+                    options: ParsingUtils.EvmSerializerOptions
+                )!;
+                tcs.SetResult(response);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.Position = 0;
             }
-            while(!receiveResult.EndOfMessage);
-
-            var msBuffer = ms.GetBuffer().AsSpan()[0..(int) ms.Length];
-            int requestId = ExtractRequestIdFromJson(msBuffer);
-
-            var (_, responseType, tcs) = _pendingRequests.First(x => x.RequestId == requestId);
-            object? response = JsonSerializer.Deserialize(msBuffer, responseType)!;
-            tcs.SetResult(response);
-
-            ms.Seek(0, SeekOrigin.Begin);
-            ms.Position = 0;
+        }
+        catch(Exception ex)
+        {
+            foreach(var (_, _, tcs) in _pendingRequests)
+            {
+                tcs.SetException(ex);
+            }
         }
     }
     private static int ExtractRequestIdFromJson(ReadOnlySpan<byte> jsonSpan)
@@ -70,10 +84,10 @@ public class WssJsonRpcTransport(Uri uri) : IRPCTransport
         var reader = new Utf8JsonReader(jsonSpan);
         while(reader.Read())
         {
-            if(reader.TokenType == JsonTokenType.PropertyName && reader.ValueTextEquals(nameof(JsonRpcResponse<object>.Id)))
+            if(reader.TokenType == JsonTokenType.PropertyName && reader.ValueTextEquals("id"))
             {
                 reader.Read();
-                return reader.GetInt32();
+                return int.Parse(reader.GetString()!.AsSpan()[2..], System.Globalization.NumberStyles.HexNumber);
             }
         }
         throw new InvalidOperationException("Id property not found");
