20c289c5473bc6863a68a62ea46ee1f68a1790e1
Playwo <eliaswolf2001@t-online.de>
2024-12-14 12:43:57 +0000

    feat: CancellationToken in EventFilters

--- src/EtherSharp/Events/Filter/EventFilter.cs
+++ src/EtherSharp/Events/Filter/EventFilter.cs
@@ -8,9 +8,9 @@ internal class EventFilter<TEvent>(IRpcClient client, string filterId) : IEventF
     private readonly IRpcClient _client = client;
     private readonly string _filterId = filterId;
 
-    public async Task<TEvent[]> GetChangesAsync()
+    public async Task<TEvent[]> GetChangesAsync(CancellationToken cancellationToken)
     {
-        var rawResults = await _client.EthGetEventFilterChangesAsync(_filterId);
+        var rawResults = await _client.EthGetEventFilterChangesAsync(_filterId, cancellationToken);
         return rawResults.Select(TEvent.Decode).ToArray();
     }
 
--- src/EtherSharp/Events/Filter/IEventFilter.cs
+++ src/EtherSharp/Events/Filter/IEventFilter.cs
@@ -2,5 +2,5 @@
 public interface IEventFilter<TEvent> : IAsyncDisposable
     where TEvent : ITxEvent<TEvent>
 {
-    public Task<TEvent[]> GetChangesAsync();
+    public Task<TEvent[]> GetChangesAsync(CancellationToken cancellationToken = default);
 }
