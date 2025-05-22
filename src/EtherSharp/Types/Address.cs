b070c93b9a3c642a6850c2bcd16e0b7ecdf22e0c
PoolPirate <eliaswolf1609@gmail.com>
2025-05-22 16:31:44 +0200

    feat: Add Zero address & add XML comments to address

--- src/EtherSharp/Types/Address.cs
+++ src/EtherSharp/Types/Address.cs
@@ -1,11 +1,26 @@
 ﻿using System.Globalization;
 
 namespace EtherSharp.Types;
+/// <summary>
+/// Represents an Ethereum compatible address.
+/// </summary>
 public class Address
 {
+    /// <summary>
+    /// Returns the Zero address.
+    /// </summary>
+    public static Address Zero { get; } = new Address("0x0000000000000000000000000000000000000000", new byte[20]);
+
     private readonly byte[] _addressBytes;
 
+    /// <summary>
+    /// Returns the string representation of the address.
+    /// </summary>
     public string String { get; }
+
+    /// <summary>
+    /// Returns the binary representation of the address.
+    /// </summary>
     public ReadOnlySpan<byte> Bytes => _addressBytes;
 
     private Address(string s, byte[] b)
@@ -14,6 +29,11 @@ public class Address
         _addressBytes = b;
     }
 
+    /// <summary>
+    /// Creates an <see cref="Address"/> instance given its string representation.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
     public static Address FromString(string s)
     {
         if(s.StartsWith("0x", false, CultureInfo.InvariantCulture))
@@ -27,6 +47,27 @@ public class Address
         }
     }
 
+    /// <summary>
+    /// Creates an <see cref="Address"/> instance given its binary representation.
+    /// </summary>
+    /// <param name="b"></param>
+    /// <returns></returns>
     public static Address FromBytes(ReadOnlySpan<byte> b)
         => new Address($"0x{Convert.ToHexString(b)}", b.ToArray());
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+        => obj is Address other && other._addressBytes.SequenceEqual(_addressBytes);
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+        => String.GetHashCode();
+
+    /// <inheritdoc/>
+    public static bool operator ==(Address a, Address b)
+        => Equals(a, b);
+
+    /// <inheritdoc/>
+    public static bool operator !=(Address a, Address b)
+        => !Equals(a, b);
 }
