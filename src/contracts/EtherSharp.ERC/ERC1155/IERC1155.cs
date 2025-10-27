e33d17193741438234aa41ab3a7f826bf57ef33c
PoolPirate <eliaswolf1609@gmail.com>
2025-10-28 00:07:31 +0100

    style: use shorthand syntax

--- src/contracts/EtherSharp.ERC/ERC1155/IERC1155.cs
+++ src/contracts/EtherSharp.ERC/ERC1155/IERC1155.cs
@@ -1,7 +1,6 @@
 ﻿using EtherSharp.Contract;
 
 namespace EtherSharp.ERC.ERC1155;
+
 [AbiFile("erc1155.abi.json")]
-public partial interface IERC1155 : IEVMContract
-{
-}
+public partial interface IERC1155 : IEVMContract;
--- src/contracts/EtherSharp.ERC/ERC721/IERC721.cs
+++ src/contracts/EtherSharp.ERC/ERC721/IERC721.cs
@@ -1,7 +1,6 @@
 ﻿using EtherSharp.Contract;
 
 namespace EtherSharp.ERC.ERC721;
+
 [AbiFile("erc721.abi.json")]
-public partial interface IERC721 : IEVMContract
-{
-}
+public partial interface IERC721 : IEVMContract;
