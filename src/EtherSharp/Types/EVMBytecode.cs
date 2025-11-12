5ddf38f5113286b87d4039305d6003ca45bb68f9
PoolPirate <eliaswolf1609@gmail.com>
2025-11-12 17:25:09 +0100

    feat: use less strict implementation checking in bytecode

--- src/EtherSharp/Types/EVMBytecode.cs
+++ src/EtherSharp/Types/EVMBytecode.cs
@@ -2,61 +2,39 @@
 
 public class EVMBytecode(Address address, ReadOnlyMemory<byte> bytecode)
 {
+    private const byte PUSH4_OPCODE = 0x63;
+    private const byte LT_OPCODE = 0x10;
+    private const byte GT_OPCODE = 0x11;
+    private const byte EQ_OPCODE = 0x14;
+    private const byte SUB_OPCODE = 0x03;
+    private static ReadOnlySpan<byte> ComparisonOpcodes => [LT_OPCODE, GT_OPCODE, EQ_OPCODE, SUB_OPCODE];
+
     public Address Address { get; } = address;
     public ReadOnlyMemory<byte> Bytecode { get; } = bytecode;
-    private readonly List<ReadOnlyMemory<byte>> _functionSelectors = FindFunctionSelectors(bytecode);
 
     public bool HasFunctions(params IEnumerable<ReadOnlyMemory<byte>> selectors)
     {
+        Span<byte> prefixedSelector = stackalloc byte[5];
         foreach(var requiredSelector in selectors)
         {
-            bool found = false;
+            prefixedSelector[0] = PUSH4_OPCODE;
+            requiredSelector.Span.CopyTo(prefixedSelector[1..]);
 
-            foreach(var existingSelector in _functionSelectors)
+            int index = Bytecode.Span.IndexOf(prefixedSelector);
+
+            if(index == -1 || Bytecode.Length < (index + 7))
             {
-                if(requiredSelector.Span.SequenceEqual(existingSelector.Span))
-                {
-                    found = true;
-                    break;
-                }
+                return false;
             }
 
-            if(!found)
+            byte suffixByte = Bytecode.Span[index + 5];
+
+            if(!ComparisonOpcodes.Contains(suffixByte))
             {
                 return false;
             }
         }
 
         return true;
-    }
-
-    private static List<ReadOnlyMemory<byte>> FindFunctionSelectors(ReadOnlyMemory<byte> bytecode)
-    {
-        var results = new List<ReadOnlyMemory<byte>>();
-
-        int calltableStartIndex = bytecode.Span.IndexOf((byte) 0x5B) + 1;
-
-        bytecode = bytecode[calltableStartIndex..];
-
-        while(true)
-        {
-            int push4Index = bytecode.Span.IndexOf((byte) 0x63);
-
-            if(push4Index == -1)
-            {
-                break;
-            }
-
-            bytecode = bytecode[(push4Index + 1)..];
-
-            if(push4Index > 20)
-            {
-                break;
-            }
-
-            results.Add(bytecode[0..4]);
-        }
-
-        return results;
     }
 }
