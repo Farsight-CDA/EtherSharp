67a215d3c4cb9b5834e14942852fdb853ce503b6
PoolPirate <eliaswolf1609@gmail.com>
2025-12-10 14:48:39 +0100

    fix: properly escape all keywords

--- src/EtherSharp.Generator/Util/NameUtils.cs
+++ src/EtherSharp.Generator/Util/NameUtils.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Text;
 
 namespace EtherSharp.Generator.Util;
+
 public static class NameUtils
 {
     public static string ToValidParameterName(string name)
@@ -54,19 +55,25 @@ public static class NameUtils
         return name;
     }
 
+    private static readonly HashSet<string> _reservedKeywords = new(
+        StringComparer.Ordinal)
+    {
+        "abstract","as","base","bool","break","byte","case","catch","char",
+        "checked","class","const","continue","decimal","default","delegate","do",
+        "double","else","enum","event","explicit","extern","false","finally","fixed",
+        "float","for","foreach","goto","if","implicit","in","int","interface","internal",
+        "is","lock","long","namespace","new","null","object","operator","out","override",
+        "params","private","protected","public","readonly","ref","return","sbyte",
+        "sealed","short","sizeof","stackalloc","static","string","struct","switch","this",
+        "throw","true","try","typeof","uint","ulong","unchecked","unsafe","ushort",
+        "using","virtual","void","volatile","while"
+    };
     public static string EscapeVariableName(string name)
-        => name switch
-        {
-            "params" => "parameters",
-            "namespace" => "@namespace",
-            "switch" => "@switch",
-            "int" => "@int",
-            "string" => "@string",
-            "operator" => "@operator",
-            "delegate" => "@delegate",
-            "default" => "@default",
-            _ => name,
-        };
+        => name is null
+            ? throw new ArgumentNullException(nameof(name))
+            : _reservedKeywords.Contains(name)
+                ? "@" + name
+                : name;
 
     public static string FullyQualifiedTypeName(ITypeSymbol symbol)
     {
