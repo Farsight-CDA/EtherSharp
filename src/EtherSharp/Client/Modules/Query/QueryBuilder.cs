20be3e68779950bf3057c87f456f08c620b4c08f
PoolPirate <eliaswolf1609@gmail.com>
2026-01-03 13:55:09 +0100

    feat: add utility methods to QueryBuilder

--- src/EtherSharp/Client/Modules/Query/QueryBuilder.cs
+++ src/EtherSharp/Client/Modules/Query/QueryBuilder.cs
@@ -29,4 +29,34 @@ public partial class QueryBuilder<TQuery> : IQuery<List<TQuery>>, IQueryBuilder<
         }
         return results;
     }
+
+    public QueryBuilder<TQuery> AddQuery(IQuery<TQuery> c1)
+    {
+        int resultIndex = _queries.Count;
+
+        int o1 = _queries.Count - resultIndex;
+        _queries.AddRange(c1.Queries);
+        _resultSelectorFunctions.Add((resultIndex, x => c1.ReadResultFrom(x[o1..])));
+        return this;
+    }
+
+    public QueryBuilder<TQuery> AddQueries(params ReadOnlySpan<IQuery<TQuery>> queries)
+    {
+        foreach(var query in queries)
+        {
+            AddQuery(query);
+        }
+
+        return this;
+    }
+
+    public QueryBuilder<TQuery> AddQueries<T1>(Func<T1, TQuery> mapping, params ReadOnlySpan<IQuery<T1>> queries)
+    {
+        foreach(var query in queries)
+        {
+            AddQuery(query, mapping);
+        }
+
+        return this;
+    }
 }
