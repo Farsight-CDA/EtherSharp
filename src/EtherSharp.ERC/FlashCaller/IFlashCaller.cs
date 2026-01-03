185880cfead90f7d1b90d3a5e3516816363bb3a6
PoolPirate <eliaswolf1609@gmail.com>
2026-01-03 14:44:11 +0100

    feat: add flash caller contracts

---
+++ src/EtherSharp.ERC/FlashCaller/IFlashCaller.cs
@@ -0,0 +1,7 @@
+﻿using EtherSharp.Contract;
+
+namespace EtherSharp.ERC.FlashCaller;
+
+[AbiFile("flash-caller.abi.json")]
+[BytecodeFile("flash-caller.bytecode")]
+public partial interface IFlashCaller : IEVMContract;
