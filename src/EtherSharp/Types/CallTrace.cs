aa8dfd3eacb020fa79e24ae1f937265745b5f5a2
PoolPirate <eliaswolf1609@gmail.com>
2026-01-08 21:46:47 +0100

    feat: make CallTrace enumerable

--- src/EtherSharp/Types/CallTrace.cs
+++ src/EtherSharp/Types/CallTrace.cs
@@ -1,4 +1,5 @@
 ﻿using EtherSharp.Numerics;
+using System.Collections;
 
 namespace EtherSharp.Types;
 
@@ -12,4 +13,55 @@ public record CallTrace(
     byte[] Output,
     CallType Type,
     CallTrace[]? Calls
-);
+) : IEnumerable<CallTrace>
+{
+    public Enumerator GetEnumerator()
+        => new Enumerator(this);
+
+    IEnumerator<CallTrace> IEnumerable<CallTrace>.GetEnumerator()
+        => GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+
+    public struct Enumerator : IEnumerator<CallTrace>
+    {
+        private readonly Stack<CallTrace> _stack;
+        private CallTrace? _current;
+
+        internal Enumerator(CallTrace root)
+        {
+            _stack = new Stack<CallTrace>();
+            _stack.Push(root);
+            _current = null;
+        }
+
+        public readonly CallTrace Current => _current!;
+        readonly object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if(!_stack.TryPop(out _current))
+            {
+                return false;
+            }
+
+            if(_current.Calls is not null)
+            {
+                for(int i = _current.Calls.Length - 1; i >= 0; i--)
+                {
+                    _stack.Push(_current.Calls[i]);
+                }
+            }
+
+            return true;
+        }
+
+        public readonly void Reset()
+            => throw new NotSupportedException();
+
+        public readonly void Dispose()
+        {
+        }
+    }
+}
