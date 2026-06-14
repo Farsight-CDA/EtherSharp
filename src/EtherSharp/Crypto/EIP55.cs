37268d2d10a36f040f0d84a150fb3944a23d31f5
PoolPirate <eliaswolf1609@gmail.com>
2026-06-15 00:34:56 +0200

    perf: use String.Create over stack buffer and copy

--- src/EtherSharp/Crypto/EIP55.cs
+++ src/EtherSharp/Crypto/EIP55.cs
@@ -15,27 +15,25 @@ public static class EIP55
     /// <param name="address"></param>
     /// <returns></returns>
     public static string FormatAddress(in Address address)
-    {
-        var addressBytes = address.DangerousGetReadOnlySpan();
-        Span<byte> asciiBytes = stackalloc byte[40];
-        WriteLowercaseAsciiHex(addressBytes, asciiBytes);
-
-        Span<byte> hash = stackalloc byte[32];
-        Keccak256.TryHashData(asciiBytes, hash);
-
-        Span<char> chars = stackalloc char[42];
-        chars[0] = '0';
-        chars[1] = 'x';
-
-        for(int i = 0; i < addressBytes.Length; i++)
+        => String.Create(Address.STRING_LENGTH, address, static (chars, state) =>
         {
-            byte value = addressBytes[i];
-            chars[(i * 2) + 2] = ApplyChecksumCase(ToLowerHexChar(value >> 4), hash[i], 4);
-            chars[(i * 2) + 3] = ApplyChecksumCase(ToLowerHexChar(value & 0x0F), hash[i], 0);
-        }
+            var addressBytes = state.DangerousGetReadOnlySpan();
+            Span<byte> asciiBytes = stackalloc byte[40];
+            WriteLowercaseAsciiHex(addressBytes, asciiBytes);
 
-        return new string(chars);
-    }
+            Span<byte> hash = stackalloc byte[32];
+            Keccak256.TryHashData(asciiBytes, hash);
+
+            chars[0] = '0';
+            chars[1] = 'x';
+
+            for(int i = 0; i < addressBytes.Length; i++)
+            {
+                byte value = addressBytes[i];
+                chars[(i * 2) + 2] = ApplyChecksumCase(ToLowerHexChar(value >> 4), hash[i], 4);
+                chars[(i * 2) + 3] = ApplyChecksumCase(ToLowerHexChar(value & 0x0F), hash[i], 0);
+            }
+        });
 
     private static void WriteLowercaseAsciiHex(ReadOnlySpan<byte> bytes, Span<byte> destination)
     {
