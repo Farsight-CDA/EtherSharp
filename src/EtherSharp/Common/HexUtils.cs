0e7c0b13a5b5ea1faa15dcc15151538d62777321
PoolPirate <eliaswolf1609@gmail.com>
2026-06-15 00:49:33 +0200

    perf: avoid closure

--- src/EtherSharp/Common/HexUtils.cs
+++ src/EtherSharp/Common/HexUtils.cs
@@ -3,7 +3,7 @@
 internal static class HexUtils
 {
     public static string ToPrefixedHexString(ReadOnlySpan<byte> data)
-        => String.Create((data.Length * 2) + 2, data, (span, state) =>
+        => String.Create((data.Length * 2) + 2, data, static (span, state) =>
         {
             span[0] = '0';
             span[1] = 'x';
