950eb527d976dce810463004e86dde7eec1f62e8
PoolPirate <eliaswolf1609@gmail.com>
2026-07-10 18:26:28 +0000

    fix: avoid continuation from blocking event loop

--- src/EtherSharp/RPC/Transport/Json/JsonRpcResponseHandler.cs
+++ src/EtherSharp/RPC/Transport/Json/JsonRpcResponseHandler.cs
@@ -4,6 +4,11 @@ namespace EtherSharp.RPC.Transport.Json;
 
 internal sealed class JsonRpcResponseHandler<TResult> : TaskCompletionSource<JsonRpcResponse<TResult>>, IJsonRpcResponseHandler
 {
+    public JsonRpcResponseHandler()
+        : base(TaskCreationOptions.RunContinuationsAsynchronously)
+    {
+    }
+
     public void SetResponse(ReadOnlySpan<byte> json, JsonSerializerOptions options)
     {
         var response = JsonSerializer.Deserialize<JsonRpcResponse<TResult>>(json, options);
