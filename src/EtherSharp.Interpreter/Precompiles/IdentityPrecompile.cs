54f01cadfa20296f9f988d6076cbdc8610c2ce40
PoolPirate <eliaswolf1609@gmail.com>
2026-09-04 13:24:50 +0200

    feat: Identity precompile

---
+++ src/EtherSharp.Interpreter/Precompiles/IdentityPrecompile.cs
@@ -0,0 +1,17 @@
+using EtherSharp.Interpreter.Runtime;
+using EtherSharp.Types;
+
+namespace EtherSharp.Interpreter.Precompiles;
+
+/// <summary>
+/// Implements the EVM identity precompile.
+/// </summary>
+public sealed class IdentityPrecompile : IPrecompile
+{
+    /// <inheritdoc/>
+    public Address Address { get; } = Address.FromString("0x0000000000000000000000000000000000000004");
+
+    /// <inheritdoc/>
+    public ValueTask<TxCallResult> ExecuteAsync(IInterpreterHost host, PrecompileCall call)
+        => ValueTask.FromResult(new TxCallResult(true, call.Input.ToArray()));
+}
